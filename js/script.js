d90a044a935cad9f2261d85eddcfe0f621c3fe8d
Debora Rudzińska <deborar@gazeta.pl>
2019-08-10 22:07:59 +0200

    Add clickedElement

--- js/script.js
+++ js/script.js
@@ -5,20 +5,24 @@ document.getElementById('test-button').addEventListener('click', function(){
   });
   */
  const titleClickHandler = function(event){
+  const clickedElement = this;
   console.log('Link was clicked!');
-  console.log('MouseEvent');
+  
+  clickedElement.classList.add('active');
 
 
-  /* remove class 'active' from all article links  */
+  /* [DONE] remove class 'active' from all article links  */
   const activeLinks = document.querySelectorAll('.titles a.active');
 
   for(let activeLink of activeLinks){
   activeLink.classList.remove('active');
 }
 
-  /* add class 'active' to the clicked link */
+  /* [IN PROGRESS] add class 'active' to the clicked link */
 
-  /* remove class 'active' from all articles */
+  console.log('clickedElement:', clickedElement);
+
+  /* [DONE] remove class 'active' from all articles */
   const activeArticles = document.querySelectorAll('a.active');
 
   for(let activeArticle of activeArticles){
